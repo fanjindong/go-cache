870fca4160faab24407bf1190fec7eaa05ee8a0c
fanjindong <fanjindong@100.me>
2021-08-04 15:14:08 +0800

    fix:

--- shard.go
+++ shard.go
@@ -93,9 +93,6 @@ func (c *memCacheShard) ttl(k string) (time.Duration, bool) {
 }
 
 func (c *memCacheShard) checkExpire() {
-	if c == nil {
-		return
-	}
 	var expiredKeys []string
 	c.lock.RLock()
 	for k, item := range c.hashmap {
